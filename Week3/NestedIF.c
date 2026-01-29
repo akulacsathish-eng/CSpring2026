0e0ebbcc84104095c4226d5d1879b1ee6f33ea9c
akulacsathish-eng <akulac.sathish@gmail.com>
2026-01-29 16:32:30 +0000

    nested if

---
+++ Week3/NestedIF.c
@@ -0,0 +1,42 @@
+/* Write a program to classify the entered grade of a student
+ 
+ A - 90 =>
+ B- 80 =>
+ C- 70 =>
+ D- 60 =>
+ E - 50 =>
+ F     <50
+*/
+#include<stdio.h>
+
+int main()
+{
+
+    float studGrade;
+
+    printf("Enter student Grade:");
+    scanf("%f",&studGrade);
+
+    if(studGrade>=90)
+    {
+            printf("\nGrade = A\n");
+    }
+    else if(studGrade>=80)
+    {
+        printf("\nGrade = B\n");
+    }
+    else if(studGrade>=70)
+    {
+        printf("\nGrade = C\n");
+    }
+    else if(studGrade>=60)
+    {
+        printf("\nGrade = D\n");
+    }
+    else if(studGrade>=50)
+    {
+        printf("\nGrade = E\n");
+    }
+    else
+        printf("\nGrade = F\n");
+}
